f9d9d27b4d724db94b0abc8cda38fe2ed51b426f
Robert Mayo <robert.mayo.ch@gmail.com>
2023-07-11 12:24:57 +0100

    completed lab and extensions

---
+++ bus_lab_start_code/bus.py
@@ -0,0 +1,24 @@
+class Bus:
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+        self.passengers = []
+    
+    def drive(self):
+        return "Brum brum"
+    def passenger_count(self):
+        return len(self.passengers)
+    
+    def pick_up(self, passenger_1):
+        self.passengers.append(passenger_1)
+
+    def drop_off(self, passenger_2):
+        self.passengers.remove(passenger_2)
+
+    def empty_bus(self):
+        self.passengers.clear()
+
+    def pick_up_from_stop(self, bus_stop_1):
+        for waiting_passengers in bus_stop_1.queue:
+            self.passengers.append(waiting_passengers)
+        bus_stop_1.clear()
